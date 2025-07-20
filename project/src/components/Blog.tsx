4b8e1ecba2a2f3207742953a41b04045d3189360
kuaya1 <Workkuaya@gmail.com>
2025-07-20 02:15:00 -0400

    blog lists

--- project/src/components/Blog.tsx
+++ project/src/components/Blog.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import blogPosts from '../data/blogPosts';
 import BlogPost from './BlogPost';
-import BlogList from './BlogList';
+import BlogPage from './BlogPage';
 import { useLocation } from 'react-router-dom';
 import SEOMetadata from './SEOMetadata';
 
@@ -40,7 +40,7 @@ const Blog = () => {
             keywords="Starlink blog, satellite internet resources, Starlink installation guides, professional Starlink setup"
             ogImage="https://images.unsplash.com/photo-1446776811953-b23d57bd21aa?auto=format&fit=crop&w=1200&q=80"
           />
-          <BlogList posts={blogPosts} />
+          <BlogPage posts={blogPosts} />
         </>
       )}
     </div>
